2a3f7646fa53e961665510b1501a1e6c47e824c6
Kuba Korzeniowski <kubix007@gmail.com>
2015-10-26 12:29:13 +0100

    zad 20, 21, 22, ogarnac HAVING

--- oracle_LISTA_2.sql
+++ oracle_LISTA_2.sql
@@ -1,6 +1,8 @@
+ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
+
 --17
 SELECT k.pseudo "POLUJE W POLU", k.przydzial_myszy, b.nazwa
-FROM Kocury k JOIN Bandy b on k.nr_bandy=b.nr_bandy
+FROM Kocury k JOIN Bandy b ON k.nr_bandy=b.nr_bandy
 WHERE NVL(k.przydzial_myszy,0) > 50 AND (b.teren='POLE' OR b.teren='CALOSC');
 --
 --select k1.pseudo "sz", k1.nr_bandy "b" from
@@ -24,3 +26,24 @@ LEFT JOIN Kocury k4 on k3.szef = k4.pseudo
 WHERE k1.funkcja = 'KOT' OR k1.funkcja = 'MILUSIA';
 
 --20
+SELECT k.imie "Imie kotki", b.nazwa "Nazwa bandy", w.imie_wroga "Imie wroga", w.stopien_wrogosci "Ocena wroga", wk.data_incydentu "Data inc."
+FROM Kocury k
+LEFT JOIN Bandy b ON k.nr_bandy = b.nr_bandy
+LEFT JOIN Wrogowie_Kocurow wk ON k.pseudo = wk.pseudo
+LEFT JOIN Wrogowie w ON wk.imie_wroga = w.imie_wroga
+WHERE plec = 'D' and data_incydentu > TO_DATE('2007-01-01','yyyy-mm-dd')
+ORDER BY k.imie;
+
+--21
+SELECT b.nazwa "Nazwa bandy", COUNT(DISTINCT k.pseudo) "Koty z wrogami"
+FROM Wrogowie_Kocurow wk
+LEFT JOIN Kocury k ON wk.pseudo = k.pseudo
+LEFT JOIN Bandy b ON k.nr_bandy = b.nr_bandy
+GROUP BY b.nazwa;
+
+--22 ogarn¹æ HAVING
+SELECT k.funkcja, k.pseudo "Pseudonim kota", COUNT(*) "Liczba wrogow"
+FROM Wrogowie_Kocurow wk
+LEFT JOIN Kocury k ON wk.pseudo = k.pseudo
+GROUP BY k.funkcja, k.pseudo
+HAVING COUNT(*) > 1;
